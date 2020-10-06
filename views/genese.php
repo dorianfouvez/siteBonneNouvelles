bacde6c5cb4dc416bf017839093f50ada1182e2d
Dorian <dorian.fouvez@student.vinci.be>
2020-10-06 18:33:37 +0200

    test methode controller :: index

--- views/genese.php
+++ views/genese.php
@@ -1,6 +1,6 @@
 <div id="contenu">
 	<h2>La Genèse</h2>
-	<p>Bienvenue sur la page de la genèse!!</p>
+	<p>Bienvenue sur la page de la genèse!</p>
 	<p>Cette page est demandée grâce à la valeur 'genese' du paramètre 'action' passé par la méthode GET, visible dans l'URL /index.php?action=genese.</p>
 	<table id="tableBalises">
 		<thead>
